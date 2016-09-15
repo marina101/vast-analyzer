33e785b1d43ac40461c201d508528e37f175a171
Marina Chirchikova <marina.chirchikova@adgear.com>
2016-09-15 15:33:21 -0400

    Fix style

--- lib/vpaid_parser/errors.rb
+++ lib/vpaid_parser/errors.rb
@@ -1,8 +1,8 @@
-module VpaidParser	
-	  class Error < StandardError; end
-	  class ErrorOpeningUrl < Error; end
-	  class WrapperRedirectError < Error; end
-	  class NotVastError < Error; end
-	  class WrapperDepthError < Error; end
-	
+# frozen_string_literal: true
+module VpaidParser
+  class Error < StandardError; end
+  class ErrorOpeningUrl < Error; end
+  class WrapperRedirectError < Error; end
+  class NotVastError < Error; end
+  class WrapperDepthError < Error; end
 end
